6b36e3c14f705f358ad8b07f8ce59db5524ae542
Parth Saliya <parthsaliya63@gmail.com>
2024-10-02 19:21:11 +0530

    ok

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -92,7 +92,7 @@ const loginUser = async (req, res) => {
         userName: checkUser.userName,
       },
       "ewff44f4fwes",
-      { expiresIn: "60m" }
+      { expiresIn: "1m" }
     );
 
     res.cookie("token", token, { httpOnly: true, secure: false }).json({
